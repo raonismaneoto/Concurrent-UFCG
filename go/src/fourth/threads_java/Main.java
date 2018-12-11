b303632009f9625b1467e458282c9402dce2c7e2
vsseixaso <vinicius.oliveira@ccc.ufcg.edu.br>
2018-12-10 23:52:03 -0300

    Update and rename Trigger.java to Main.java

--- go/src/fourth/threads_java/Main.java
+++ go/src/fourth/threads_java/Main.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class Trigger {
+public class Main {
 	private static long getMemory() {
 		return Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 	}
